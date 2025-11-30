364dea38adf84ce318009ac5f0ce77da2ed0a818
Michael S. Collins <mscollins@barracuda.com>
2025-11-30 04:02:50 +0000

    Tweak install

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ INSTALL_DIR=${INSTALL_DIR:-/opt/kast}
 
 # Install Node.js (includes npm)
 apt install -y ca-certificates curl gnupg rsync
-apt install -y firefox-esr git golang gpg htop nginx npm openjdk-21-jre python3 python3-venv sslscan testssl.sh wafw00f whatweb
+apt install -y firefox-esr git golang gpg htop nginx openjdk-21-jre python3 python3-venv sslscan testssl.sh wafw00f whatweb
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
